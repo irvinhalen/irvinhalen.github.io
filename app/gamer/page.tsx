5ac8a7b15b60aca438a8fa2b22752094b5fbfe6c
irvinhalen <irvinhalen.larawan@gmail.com>
2024-08-16 09:49:51 +0800

    wrote a little for gamer

--- app/gamer/page.tsx
+++ app/gamer/page.tsx
@@ -4,6 +4,11 @@ import Pokémon from '../../public/Pokémon.svg';
 import Mario from '../../public/Mario.svg';
 import Zelda from '../../public/Zelda.svg';
 import Minecraft from '../../public/Minecraft.svg';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Gamer'
+};
 
 export default function Page() {
     return (
@@ -13,7 +18,7 @@ export default function Page() {
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">Super Smash Bros.</div>
                     <p className="text-gray-700 text-base">
-                        Talk about Super Smash Bros. being a crossover of your favorite characters and game series. Started from a fan game (Super Smash Flash), second hand Brawl from cousins, for the Nintendo 3DS (playing the demo and buying the game), for the Wii U from a friend (arbor), Ultimate with your first owned console, the Nintendo Switch.
+                        Super Smash Bros. is one of my favorite game series of all time. My journey with these games started from a fan game of all things, when my brother and I would play Super Smash Flash. Later on my cousins gave us several second hand games and consoles, one of them being the Wii Mini and a copy of Super Smash Bros. Brawl where I finally got a real taste of what this franchise had to offer, seeing Sonic in this game and the Subspace Emissary was mind blowing. When Smash for the Nintendo 3DS was close to releasing we played the demo version endlessly, we wouldn&apos;t even turn our 2DS and 3DS XL off because the demo had limited times of play, this is when I paid closer attention to guest characters and reveal trailers. I was able to play Super Smash Bros. for the Wii U in college because I &quot;arbor&quot; my friend&apos;s Wii U and his copy of the game, this is when I started to pay attention to the competitive scene and I had a lot of fun friendly and sweaty games with my friends. The first home console I officially owned was the Nintendo Switch, I saved 10 Peso Coins which reached around 4k, the rest I had to ask help from my mom since I was still in college (8 years), Super Smash Bros. Ultimate with ever fighter coming back and some new faces, we played this endlessly, I had a lot of friendly and sweaty games with my friends, I even joined a tournament in Cebu where I got 3rd place.
                     </p>
                 </div>
             </div>
@@ -23,7 +28,7 @@ export default function Page() {
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">Pokémon</div>
                         <p className="text-gray-700 text-base">
-                            Pokémon Emerald, Pokémon Omega Ruby and Pokémon Alpha Sapphire (ORAS) with bro, Pokémon GO with my girl, brothers, friends (college time).
+                            Pokémon Emerald, Pokémon Omega Ruby and Pokémon Alpha Sapphire (ORAS) with bro and we begged mum to get us a 2DS and a 3DS XL for our birthdays, Pokémon GO with my girl, brothers, friends (college time).
                         </p>
                     </div>
                 </div>
@@ -32,7 +37,7 @@ export default function Page() {
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">Super Mario</div>
                         <p className="text-gray-700 text-base">
-                            Always makes sounds based on Super Mario theme. Completed a fake copy of Super Mario Bros. 3 Super Mario Advance 4, Mario and Luigi Superstar Saga, Super Mario Odyssey.
+                            Always makes sounds based on Super Mario theme. Completed a fake copy of Super Mario Bros. 3 Super Mario Advance 4, Mario and Luigi Superstar Saga, Super Mario Odyssey 100%.
                         </p>
                     </div>
                 </div>
@@ -43,7 +48,7 @@ export default function Page() {
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">The Legend of Zelda</div>
                         <p className="text-gray-700 text-base">
-                            BotW & TotK hyped with late friend.
+                            Multiplayer Triforce Heroes , Link Between Worlds 100%, BotW & TotK hyped with late friend.
                         </p>
                     </div>
                 </div>
