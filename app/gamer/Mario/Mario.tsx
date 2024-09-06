78a7dd7fe1c9d526dcf1e3a8a9a71d2eae569b5f
irvinhalen <irvinhalen.larawan@gmail.com>
2024-09-06 09:55:25 +0800

    Super Mario

---
+++ app/gamer/Mario/Mario.tsx
@@ -0,0 +1,27 @@
+import icon from '../../../public/Mario/Mario.svg';
+    import Mario1 from '../../../public/Mario/Mario1.jpg';
+import Image from 'next/image';
+
+function Mario() {
+  return (
+    <div className='flex flex-row gap-5 h-80 w-full'>
+        <div className="flex flex-row max-w rounded overflow-hidden shadow-lg justify-center items-center w-4/5 relative">
+            <Image src={Mario1} alt='Mario' loading='lazy' layout='fill' objectFit='cover' objectPosition='center' />
+        </div>
+        <div className="flex flex-row max-w rounded overflow-hidden shadow-lg justify-center items-center w-1/5 relative">
+            <Image src={icon} alt='Super Mario logo' loading='lazy' objectPosition='center' />
+        </div>
+        {/* <div className="flex flex-row max-w rounded overflow-hidden shadow-lg items-center w-3/6">
+            <Image src={Mario} alt='Super Mario logo' loading='lazy' />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">Super Mario</div>
+                <p className="text-gray-700 text-base text-justify">
+                    Always makes sounds based on Super Mario theme. Completed a fake copy of Super Mario Bros. 3 Super Mario Advance 4, Mario and Luigi Superstar Saga, Super Mario Odyssey 100%.
+                </p>
+            </div>
+        </div> */}
+    </div>
+  )
+}
+
+export default Mario;
