d544e0799f0168d88c7b68446cd96ee1fd00c909
irvinhalen <irvinhalen.larawan@gmail.com>
2024-09-10 09:25:28 +0800

    Tutorial

--- app/gamer/page.tsx
+++ app/gamer/page.tsx
@@ -5,6 +5,7 @@ import Zelda from './Zelda/Zelda';
 import Minecraft from './Minecraft/Minecraft';
 import { Metadata } from 'next';
 import Lock from './Lock';
+import Tutorial from './Tutorial';
 
 export const metadata: Metadata = {
   title: 'Gamer'
@@ -14,6 +15,7 @@ export default function Page() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between gamer-container'>
             <Lock />
+            <Tutorial />
             <Smash />
             <Pokémon />
             <Mario />
