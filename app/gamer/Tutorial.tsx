05fcc7a953a157ca624e8ffed2a190907cd4c5c3
irvinhalen <irvinhalen.larawan@gmail.com>
2024-09-11 09:29:45 +0800

    Tutorial

--- app/gamer/Tutorial.tsx
+++ app/gamer/Tutorial.tsx
@@ -3,10 +3,12 @@ function Tutorial() {
   return (
     <>
         <div className='h-svh w-svw flex flex-col gap-5 justify-center items-center bg-black'>
+            <h1>Tutorial</h1>
+            <p>Controls</p>
             <button type='button' className='text-white bg-gray-800 hover:bg-gray-900 font-large rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>understood</button>
         </div>
     </>
   )
 }
 
-export default Tutorial
+export default Tutorial;
