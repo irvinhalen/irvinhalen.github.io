ea211b00af6691eacd092b6626bf7adfe14c8c4e
irvinhalen <irvinhalen.larawan@gmail.com>
2024-09-13 14:51:11 +0800

    Tutorial

--- app/gamer/Tutorial.tsx
+++ app/gamer/Tutorial.tsx
@@ -4,7 +4,7 @@ function Tutorial() {
     <>
         <div className='h-svh w-svw flex flex-col gap-5 justify-center items-center bg-black'>
             <h1 className='text-white'>Tutorial</h1>
-            <p>Controls</p>
+            <p className='text-white'>Controls</p>
             <button type='button' className='text-white bg-gray-800 hover:bg-gray-900 font-large rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>understood</button>
         </div>
     </>
