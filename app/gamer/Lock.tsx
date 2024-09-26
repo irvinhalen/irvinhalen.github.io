3451eaa4e71ca6d7927ab06fe0fa2265c94ba1d6
irvinhalen <irvinhalen.larawan@gmail.com>
2024-09-26 09:36:58 +0800

    moved controls unlock to tutorial screen

--- app/gamer/Lock.tsx
+++ app/gamer/Lock.tsx
@@ -4,7 +4,7 @@ import { scrollDown } from './controls';
 
 function Lock() {
     const [password, setPassword] = useState<string>('');
-    const lockscreen = useRef<HTMLDivElement>(null);
+    const lockScreen = useRef<HTMLDivElement>(null);
 
     const nextScreen = () => {
         return new Promise<void>((resolve) => {
@@ -18,8 +18,8 @@ function Lock() {
     const unlock = async () => {
         if(password == '1010') {
             await nextScreen();
-            if(lockscreen.current) {
-                lockscreen.current.remove();
+            if(lockScreen.current) {
+                lockScreen.current.remove();
             }
         }else{
             setPassword('');
@@ -28,7 +28,7 @@ function Lock() {
 
     return (
         <>
-            <div id='lockscreen' ref={lockscreen} className='h-svh w-svw flex flex-col gap-5 justify-center items-center bg-black'>
+            <div id='lockScreen' ref={lockScreen} className='h-svh w-svw flex flex-col gap-5 justify-center items-center bg-black'>
                 <input type='password' placeholder='Password' className='text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' value={password} onChange={event => setPassword(event.target.value)} />
                 <button type='button' onClick={unlock} className='text-white border border-white hover:border-yellow-400 hover:text-yellow-400 font-large rounded-lg text-sm px-5 py-2.5'>Unlock</button>
             </div>
