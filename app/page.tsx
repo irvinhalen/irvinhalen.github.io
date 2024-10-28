593fbb2dde3fe604d32aef5694a647332b217da4
irvinhalen <irvinhalen.larawan@gmail.com>
2024-10-28 16:56:54 +0800

    Experiment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
-import Smash from '../public/Smash/SuperSmashBros.svg';
+import background from '../public/Mii/desk.jpg';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Howdy! 🤠</h1>
-      <p>This page is coming soon.</p>
+    <main className='flex min-h-screen flex-col items-center justify-center bg-gray-200'>
+      <div className='relative h-screen w-screen flex items-center justify-center'>
+        <Image
+          src={background}
+          alt="desk"
+          loading="lazy"
+          className="absolute top-0 right-0 bottom-0 left-0 object-contain pointer-events-none"
+        />
+        <a
+          href="#"
+          className="absolute left-[52%] right-[46%] top-[66%] bottom-[27%] bg-black"
+        >
+        </a>
+      </div>
     </main>
   );
 }
