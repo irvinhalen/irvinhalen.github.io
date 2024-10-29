ea965f9f656fb6d7819aaaba4efa8ef9f50dd409
irvinhalen <irvinhalen.larawan@gmail.com>
2024-10-29 17:15:29 +0800

    Experimentation

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import background from '../public/Mii/desk.jpg';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center bg-gray-200'>
-      <div className='relative h-screen w-screen flex items-center justify-center'>
+      <h1>Coming Soon! 💫</h1>
+      {/* <div className='relative h-screen w-screen flex items-center justify-center'>
         <Image
           src={background}
           alt="desk"
@@ -16,7 +17,7 @@ export default function Home() {
           className="absolute left-[52%] right-[46%] top-[66%] bottom-[27%] bg-black"
         >
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
