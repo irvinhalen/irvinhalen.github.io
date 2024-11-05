073e8c559066b487eeda68da064bbcf16ee5d67c
irvinhalen <irvinhalen.larawan@gmail.com>
2024-11-05 09:19:24 +0800

    Working Hitboxes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
+'use client'
 import Image from "next/image";
 import background from '../public/Mii/desk.jpg';
 import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const imageRef = useRef<HTMLImageElement>(null);
+  const linkContainer = useRef<HTMLDivElement>(null);
+
+  const updateImageSize = () => {
+    if (imageRef.current) {
+      const { width, height } = imageRef.current.getBoundingClientRect();
+      if (linkContainer.current) {
+        linkContainer.current.style.width = `${width}px`;
+        linkContainer.current.style.height = `${height}px`;
+      }
+    }
+  };
+
+  useEffect(() => {
+    updateImageSize();
+    window.addEventListener('resize', updateImageSize);
+
+    return () => {
+      window.removeEventListener('resize', updateImageSize);
+    };
+  }, []);
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-center bg-gray-200'>
       <div className='relative h-screen w-screen flex items-center justify-center'>
@@ -10,19 +34,23 @@ export default function Home() {
           src={background}
           alt="desk"
           loading="lazy"
-          className="absolute top-0 right-0 bottom-0 left-0 object-contain pointer-events-none"
+          ref={imageRef}
+          className="absolute object-contain pointer-events-none"
         />
+        <div ref={linkContainer} className='relative'>
+          <Link
+            href="https://github.com/irvinhalen"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute left-[19.7%] right-[68.8%] top-[55.7%] bottom-[29.3%] bg-black opacity-0"
+          />
           <Link
             href="/gamer"
             target="_blank"
             rel="noopener noreferrer"
-            className="absolute left-[52%] right-[46%] top-[66%] bottom-[27%] bg-black"
+            className="absolute -skew-y-6 left-[24.3%] right-[60.6%] top-[79.4%] bottom-[13.5%] bg-black opacity-0"
           />
-          <Link
-            href="#"
-            rel="noopener noreferrer"
-            className="absolute left-[66%] right-[27%] top-[52%] bottom-[46%] bg-black"
-          />
+        </div>
       </div>
     </main>
   );
