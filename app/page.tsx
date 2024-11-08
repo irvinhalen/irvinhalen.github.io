e564c292a4c2cc70098cc007e7874518337a73f5
irvinhalen <irvinhalen.larawan@gmail.com>
2024-11-08 09:45:51 +0800

    interactable objects are now functional

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
-import { useEffect, useRef, useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import hyacinth from '../public/Mii/hyacinth.png';
 import background from '../public/Mii/desk.jpg';
 import lock from '../public/Mii/icons/lock.ico';
 import key from '../public/Mii/icons/key.ico';
 import disc from '../public/Mii/icons/disc.ico';
 import folder from '../public/Mii/icons/folder.ico';
 import exit from '../public/Mii/icons/exit.ico';
+import book from '../public/Mii/book.png';
+import mug from '../public/Mii/mug.png';
+import glasses from '../public/Mii/glasses.png';
 
 export default function Home() {
   const [onFolder, setOnFolder] = useState<boolean>(false);
@@ -16,8 +20,9 @@ export default function Home() {
   const [onKey, setOnKey] = useState<boolean>(false);
   const [onDisc, setOnDisc] = useState<boolean>(false);
   const [onBlackground, setOnBlackground] = useState<boolean>(false);
+  const [objectClicked, setObjectClicked] = useState<boolean>(false);
+  const [objectName, setObjectName] = useState<string>('');
   const imageRef = useRef<HTMLImageElement>(null);
-  const introRef = useRef<HTMLVideoElement>(null);
   const linkContainer = useRef<HTMLDivElement>(null);
   const voidRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
@@ -35,7 +40,6 @@ export default function Home() {
   useEffect(() => {
     updateImageSize();
     window.addEventListener('resize', updateImageSize);
-
     return () => {
       window.removeEventListener('resize', updateImageSize);
     };
@@ -43,15 +47,56 @@ export default function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-center bg-gray-200'>
-      <div className={`z-10 absolute h-svh w-svw bg-black opacity-80 ${onBlackground ? '':'hidden'}`}>
+      <div className={`z-10 absolute top-0 bottom-0 left-0 right-0 h-svh w-svw bg-black invisible-transition ${onBlackground ? 'visible opacity-80':'invisible opacity-0'}`}>
         <div
             onClick={() => {
               setOnBlackground(false);
             }}
-            className="text-white text-5xl [text-shadow:_2px_2px_5px_rgb(80_80_80_/_80%)] cursor-pointer absolute top-[5%] right-[5%]"
+            className="z-30 select-none text-white text-5xl [text-shadow:_2px_2px_5px_rgb(80_80_80_/_80%)] cursor-pointer absolute top-[5%] right-[5%]"
         >
           ×
         </div>
+      </div>
+      <div className={`z-20 absolute flex flex-col items-center justify-center object-transition ${onBlackground && objectClicked && objectName === 'book' ? 'visible':' invisible scale-75'}`}>
+        <div className="relative h-full w-full">
+          <Image
+            src={hyacinth}
+            alt='hyacinth'
+            loading="eager"
+            height={250}
+            className="absolute top-[25%] bottom-[75%] left-[60%] right-[40%] underline text-blue-500"
+          />
+          <Link
+            href="https://github.com/irvinhalen"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute top-[77%] bottom-[23%] left-[59%] right-[41%] underline text-blue-500 text-xs"
+          >
+            https://github.com/irvinhalen
+          </Link>
+          <Image
+            src={book}
+            alt='book'
+            loading="eager"
+            height={500}
+          />
+        </div>
+      </div>
+      <div className={`z-20 absolute flex flex-col items-center justify-center object-transition ${onBlackground && objectClicked && objectName === 'mug' ? 'visible':' invisible scale-75'}`}>
+        <Image
+          src={mug}
+          alt='mug'
+          loading="eager"
+          height={500}
+        />
+      </div>
+      <div className={`z-20 absolute flex flex-col items-center justify-center object-transition ${onBlackground && objectClicked && objectName === 'glasses' ? 'visible':' invisible scale-75'}`}>
+        <Image
+          src={glasses}
+          alt='glasses'
+          loading="eager"
+          height={500}
+        />
       </div>
       <div className={`z-10 flex items-center justify-center h-svh w-svw bg-white ${onDisc ? '':'hidden'}`}>
         <Image
@@ -190,15 +235,30 @@ export default function Home() {
               )}
             </div>
           </div>
-          <Link
-            href="https://github.com/irvinhalen"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="absolute left-[19.7%] right-[68.8%] top-[55.7%] bottom-[29.3%] bg-black opacity-0"
+          <div
+            className="absolute cursor-pointer left-[9%] right-[64%] top-[65.5%] bottom-[23.5%] bg-black opacity-0"
+            style={{ transform: 'skewX(45deg) skewY(-6deg)' }}
+            onClick={() => {
+              setOnBlackground(true);
+              setObjectClicked(true);
+              setObjectName('book');
+            }}
+          />
+          <div
+            className="absolute cursor-pointer left-[19.7%] right-[68.8%] top-[55.7%] bottom-[29.3%] bg-black opacity-0"
+            onClick={() => {
+              setOnBlackground(true);
+              setObjectClicked(true);
+              setObjectName('mug');
+            }}
           />
           <div
             className="absolute cursor-pointer -skew-y-6 left-[24.3%] right-[60.6%] top-[79.4%] bottom-[13.5%] bg-black opacity-0"
-            onClick={() => setOnBlackground(true)}
+            onClick={() => {
+              setOnBlackground(true);
+              setObjectClicked(true);
+              setObjectName('glasses');
+            }}
           />
         </div>
       </div>
